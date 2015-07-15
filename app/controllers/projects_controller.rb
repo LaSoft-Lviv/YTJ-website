304e4491aa960196c2a1593ad3ac515c44f991ba
Vasyl <varkholyak@gmail.com>
2015-07-15 19:48:33 +0300

    Added configuration for sprockets.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,31 +1,26 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate, only: [:create]
+  before_action :authenticate, only: :create
 
   def index
   end
 
   def create
-      @team_member = TeamMember.find(params[:team_member_id])
-      @project = Project.new( name: params[:name], description: params[:description], image:  params[:file])
-      if @project.save
-        @project.team_members_projects.create(coordinator: true, team_member:@team_member)
-        render status: 200,
-               json: { success: true, info: "project add"}
-      else
-        render status: false,
-               json: { success: false, errors: @project.errors }
-      end
+    @team_member = TeamMember.find(params[:team_member_id])
+    @project = Project.new(name: params[:name], description: params[:description], image: params[:file])
 
+    if @project.save
+      @project.team_members_projects.create(coordinator: true, team_member: @team_member)
+
+      render json: { success: true, info: 'The project was added' }, status: 200
+    else
+      render json: { success: false, errors: @project.errors }, status: false
+    end
   end
-
-
 
   def destroy
   end
 
   def update
-
   end
-
 
 end
