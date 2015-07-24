1b80e591582d7049d0bd486128447fc147902522
SofiaNabyvanets <sofia.nabivanec@gmail.com>
2015-07-24 17:45:00 +0300

    Fix validates team member name for urk

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -3,7 +3,7 @@ class TeamMember< ActiveRecord::Base
   has_many :projects, through: :team_members_projects
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, :surname , presence: true, length:{in: 4..16 }, format: { with: /\A[a-zA-Z]+\z/}
+  validates :name, :surname , presence: true, length:{in: 4..16 }, format: { with: /\A[a-zA-ZА-Яа-яІі]+\z/}
   validates :foto, :quote, presence: true
   validates :email, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 30 },uniqueness: { case_sensitive: false }
   validates :facebook_link, format: URI::regexp(%w(http https)), allow_blank: true
