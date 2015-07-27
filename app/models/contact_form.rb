c67b4b686a800c1c3c148f0ac3b6710c512d0534
Sofia Nabyvanets <sofia.nabivanec@gmail.com>
2015-07-27 21:50:04 +0300

    Add to contact form validate message field adn alert errors

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -3,5 +3,10 @@ class ContactForm
   include ActiveModel::Validations
 
   attr_accessor :name,  :subject, :email, :message
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, :subject, :message, :email, presence: true
+  validates :name, length: {in: 3..16}
+  validates :subject, length: {minimum: 3}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 30 }
 end
