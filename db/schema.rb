13cc23f4302547d58df6bee2d53a0168103df4d0
Sofia Nabyvanets <sofia.nabivanec@gmail.com>
2015-07-27 19:31:14 +0300

    Add to migration column facebook_link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720123009) do
+ActiveRecord::Schema.define(version: 20150727162324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,10 @@ ActiveRecord::Schema.define(version: 20150720123009) do
     t.string   "name"
     t.text     "description"
     t.datetime "date_of"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "image"
+    t.string   "facebook_link"
   end
 
   create_table "team_members", force: :cascade do |t|
