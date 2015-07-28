54b186ff7482d3aab4ff6259e0d4b01207f3722b
VitaliyAndrushko <vitalika1988@gmail.com>
2015-07-28 07:42:25 +0000

    vit-last-commit

--- db/migrate/20150727162324_add_facebook_link_to_projects.rb
+++ db/migrate/20150727162324_add_facebook_link_to_projects.rb
@@ -1,5 +1,6 @@
 class AddFacebookLinkToProjects < ActiveRecord::Migration
   def change
     #add_column :projects, :facebook_link, :string
+    add_column :projects, :facebook_link, :string
   end
 end
