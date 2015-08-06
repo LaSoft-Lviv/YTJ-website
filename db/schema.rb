3449a54320cdc8431a9595972a043ae857dc956d
Vasyl <varkholyak@gmail.com>
2015-08-06 17:59:37 +0300

    Added new schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729101346) do
+ActiveRecord::Schema.define(version: 20150729132228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,14 @@ ActiveRecord::Schema.define(version: 20150729101346) do
     t.string   "name"
     t.text     "description"
     t.datetime "date_of"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image"
-    t.string   "facebook_link"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "slides", force: :cascade do |t|
