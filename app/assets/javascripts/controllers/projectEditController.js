c22aabcfb9a7c371a60d85dfa47115969b18bacb
VitaliyAndrushko <vitalika1988@gmail.com>
2015-08-12 11:22:13 +0300

    feature new forms

--- app/assets/javascripts/controllers/projectEditController.js
+++ app/assets/javascripts/controllers/projectEditController.js
@@ -1,37 +1,33 @@
 angular.module('controllers')
     .controller('ProjectEditController', ['$scope','$location','$http','$route','$routeParams','ProjectService',
-                                        function ($scope,  $location, $http, $route, $routeParams, ProjectService) {
+        function ($scope,  $location, $http, $route, $routeParams, ProjectService) {
 
-         $scope.project = {};
+        $scope.project = {};
 
         var params = $route.current.params;
+
         ProjectService.edit(params).then(function(data) {
             $scope.project.name = data.name;
             $scope.project.description = data.description;
             $scope.project.facebook_link = data.facebook_link;
-            if(data.team_members.length>0) {
-
-                $scope.project.team_member_prev_id = data.team_members[0].id;
-
-            }
-            else
-            {
+            $scope.project.image = data.image;
+            
+            if(data.team_members.length > 0) {
+              $scope.project.team_member_prev_id = data.team_members[0].id;
+              $scope.project.team_member = data.team_members[0];
+            } else {
                 $scope.project.team = null;
             }
-            $scope.project.image = data.image;
+           
             ProjectService.getAllTeamMembers().then(function (data) {
-
-               $scope.team = data.data.team;
-
-
+              $scope.team = data.data.team;
             });
-
-           ;
         });
-         $scope.updateProject = function() {
+
+        $scope.updateProject = function() {
           var form = collectFormData();
-             ProjectService.update(form, params).then(function(data) {
-                if (data.success) {
+            ProjectService.update(form, params).then(function(data) {
+              if (data.success) {
                   $location.path('/projects')
               } else {
                   if (data.errors)
@@ -63,28 +59,70 @@ angular.module('controllers')
               }
           };
 
-           var collectFormData = function() {
+          var collectFormData = function() {
             var form = new FormData();
+
             form.append('name', $scope.project.name);
             form.append('description', $scope.project.description);
             form.append('facebook_link', $scope.project.facebook_link);
 
-
-             if( $scope.project.team && $scope.project.team_member_prev_id &&$scope.project.team_member_prev_id != $scope.project.team)
+            if($scope.project.team && $scope.project.team_member_prev_id && $scope.project.team_member_prev_id != $scope.project.team)
              {
-                 form.append('team_member_id', $scope.project.team);
+                form.append('team_member_id', $scope.project.team);
                 form.append('team_member_prev_id', $scope.project.team_member_prev_id);
-             }
-             else if(!$scope.project.team_member_prev_id && $scope.project.team)
-             {
-                 form.append('team_member_id', $scope.project.team);
+             }  else if (!$scope.project.team_member_prev_id && $scope.project.team)
+             {  form.append('team_member_id', $scope.project.team);
              }
              // gather images and files
              if ($scope.project.image.fresh) {
                 form.append('image', $scope.project.image.file);
-                }
-               return form;
+             }
+             return form;
            };
+
+           $scope.getErrorName = function (error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                  return "Поле не повинно бути пустим";
+                } else if (error.pattern) {
+                    return "Введіть правильну назву";
+                  } else if (error.minlength) {
+                      return "Назва повинно містити не менше 4 символів";
+                    } else if (error.maxlength) {
+                        return "Назва повинно містити не більше 50 символів";
+                      }
+            }
+        }
+
+        $scope.getErrorDescription = function (error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                  return "Поле не повинно бути пустим";
+                } else if (error.minlength) {
+                      return "Опис повинен містити не менше 50 символів";
+                  } else if (error.maxlength) {
+                        return "Опис повинен містити не більше 1000 символів";
+                    }
+            }
+        }
+
+        $scope.getErrorFacebook = function (error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                  return "Поле не повинно бути пустим";
+                } else if (error.pattern) {
+                    return "Введіть посилання у форматі: https://www.facebook.com/posmishka.ua?fref=ts";
+                  }
+            }
+        }
+
+        $scope.getErrorOrganizator = function (error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                  return "Поле не повинно бути пустим";
+                } 
+            }
+        }
 
 
     }]);
