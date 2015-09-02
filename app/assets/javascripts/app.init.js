2d078a6b959476993fb4e9bf0b152aed2de5c611
VitaliyAndrushko <vitalika1988@gmail.com>
2015-09-02 11:23:40 +0300

    fix scroll button

--- app/assets/javascripts/app.init.js
+++ app/assets/javascripts/app.init.js
@@ -92,7 +92,6 @@ function getAttrBack() {
 
 
 $(document).ready(function() {
-    debugger;
     // This command is used to initialize some elements and make them work properly
     //$.material.init();
 
